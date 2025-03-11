5def2f6bc954ad996a0e73166e70194fc912c930
Sachin Kumar <sachinkumarskpog@gmail.com>
2025-03-11 11:38:01 +0530

    style: update styling adjustments

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const interface = document.querySelector(".interface");
     const createGameBtn = document.getElementById("createGameBtn");
-    const gameLink = document.getElementById("game-link");
 
     var config = {
         pieceTheme:
@@ -11,15 +11,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     createGameBtn.addEventListener("click", async () => {
         console.log("Create game button is clicked");
+        createGameLink();
         const response = await fetch(
             "https://online-chess-game-shwe.onrender.com/createGame"
             // "http://localhost:3000/createGame"
         );
         const data = await response.json();
-        // window.location.href = `/gamearena/${data.gameId}`;
         const link = `https://onlinechessgame.vercel.app/gamearena/${data.gameId}`;
 
         // Display game link
+        const gameLink = document.getElementById("game-link");
         gameLink.innerHTML = `Share this link: <a href="${link}" target="_blank">${link}</a>`;
     });
+
+    function createGameLink() {
+        // Create the link container div
+        const linkContainer = document.createElement("div");
+        linkContainer.classList.add("link-container");
+
+        // Create the span inside the div
+        const gameLink = document.createElement("span");
+        gameLink.id = "game-link";
+
+        // Append the span inside the container div
+        linkContainer.appendChild(gameLink);
+
+        // Append the link container div to interface section
+        interface.appendChild(linkContainer);
+    }
 });
